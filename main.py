c03280f7ce657597c1c97c7196e07bb779ee1664
Hubert1225 <164141@stud.prz.edu.pl>
2022-04-02 16:39:45 +0200

    add/main.py: getting data from Kafka 'raw' topic

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import faust
+from etls.record import MeasurementRecord
 
 app = faust.App("myapp", broker="kafka://localhost:9092")
+
+raw_topic = app.topic("raw", value_type=MeasurementRecord)
+
+@app.agent(raw_topic)
+async def data_parsing(measurements):
+    async for measurement in measurements:
+        # Parsing data and sending them to the next topic
+        pass
