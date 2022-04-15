ff12f7694d87ec48b24bba3ba6938ff1628dd567
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-04-15 23:49:31 +0200

    add/app.py: basic app
    it does basically nothing, but it's here!

---
+++ etls/app.py
@@ -0,0 +1,14 @@
+import faust
+from etls.data_parse_and_clean import parse_measurement
+
+from etls.record import MeasurementRecord, TimeStamp
+
+app = faust.App("wad", broker="kafka://100.111.43.19:9092", value_serializer="json")
+
+metrics_topic = app.topic("metrics", key_type=TimeStamp, value_type=MeasurementRecord)
+
+
+@app.agent(metrics_topic)
+async def distribute(measurements):
+    async for measurement in measurements:
+        await parse_measurement(measurement=measurement)
