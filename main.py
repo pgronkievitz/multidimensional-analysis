d1794108c45add5f1f2d04a5a58ce6a3ccd7a97e
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-04-18 14:38:19 +0200

    refactor/app.py: formatting

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import faust
-from etls.record import MeasurementRecord, ParsedRecord
-from etls.data_parse_and_clean import parse_measurement, remove_nones
+from record import MeasurementRecord, ParsedRecord
+from data_parse_and_clean import parse_measurement, remove_nones
 
 app = faust.App("myapp", broker="kafka://100.111.43.19:9091")
 
 raw_topic = app.topic("raw", value_type=MeasurementRecord)
-parsed_topic = app.topic("parsed",
-                         internal=True,
-                         partitions=1,
-                         value_type=ParsedRecord)
+parsed_topic = app.topic("parsed", internal=True, partitions=1, value_type=ParsedRecord)
 processed_topic = app.topic("processed", value_type=ParsedRecord)
+
 
 @app.agent(raw_topic)
 async def data_parsing(measurements):
     async for measurement in measurements:
         parsed = await parse_measurement(measurement)
         await parsed_topic.send(value=parsed)
+
 
 @app.agent(parsed_topic)
 async def data_cleaning(parsed_measurements):
