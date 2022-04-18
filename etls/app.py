0e5c697548662b66a689ca977e8c75eec09b6ca1
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2022-04-18 14:35:34 +0200

    update/topics: fix 'em all!

--- etls/app.py
+++ etls/app.py
@@ -1,14 +1,18 @@
 import faust
-from etls.data_parse_and_clean import parse_measurement
+from data_parse_and_clean import parse_measurement
 
-from etls.record import MeasurementRecord, TimeStamp
+from record import MeasurementRecord, ParsedRecord
 
 app = faust.App("wad", broker="kafka://100.111.43.19:9091", value_serializer="json")
 
-metrics_topic = app.topic("metrics", key_type=TimeStamp, value_type=MeasurementRecord)
+metrics_topic = app.topic("metrics", value_type=MeasurementRecord)
 
 
 @app.agent(metrics_topic)
 async def distribute(measurements):
     async for measurement in measurements:
         await parse_measurement(measurement=measurement)
+
+
+if __name__ == "__main__":
+    app.main()
